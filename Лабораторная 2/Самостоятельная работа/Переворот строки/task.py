a08d0e3b825d85f3080e9bbb09331aeb973fdcf8
alexceygryzilov <alexeygryzilov@gmail.com>
2024-06-07 08:59:11 +0300

    2.8

--- Лабораторная 2/Самостоятельная работа/Переворот строки/task.py
+++ Лабораторная 2/Самостоятельная работа/Переворот строки/task.py
@@ -2,18 +2,20 @@ from typing import List
 
 
 def reverse_string(s: List[str]) -> None:
-    #left, right = 0, len(s) - 1
-    #while left < right:
+    # left, right = 0, len(s) - 1
+    # while left < right:
     #    s[left], s[right] = s[right], s[left]
     #    left, right = left + 1, right - 1
 
-    m = len(s) // 2
-    s[:m], s[-1:-m - 1:-1] = s[-1:-m - 1:-1], s[:m]
-
+    middle = len(s) // 2
+    for i in range(middle):
+        s[i], s[-1 - i] = s[-1 - i], s[i]
 
 
 if __name__ == '__main__':
     a = [i for i in 'hello']
     reverse_string(a)
     print(a)
-
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    reverse_string(b)
+    print(b)
