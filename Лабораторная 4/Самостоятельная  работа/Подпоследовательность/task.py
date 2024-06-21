f069f0a8e5cde028cff540f3420ee35c949dbd07
alexceygryzilov <alexeygryzilov@gmail.com>
2024-06-21 15:01:32 +0300

    4.4

--- Лабораторная 4/Самостоятельная  работа/Подпоследовательность/task.py
+++ Лабораторная 4/Самостоятельная  работа/Подпоследовательность/task.py
@@ -4,20 +4,15 @@ def is_subsequence(s: str, t: str) -> bool:
 
     list_1 = [_ for _ in s]
     list_2 = [_ for _ in t]
-    list_3 = list_2.copy()
-    counter = 0
+    list_2_unique = list(dict.fromkeys(list_2))
+    list_3 = list_2_unique.copy()
 
-    for i in list_2:
-        if i in list_1:
-            counter == 1
+    for i in list_2_unique:
         if i not in list_1:
             list_3.remove(i)
-        if counter == len(list_1):
-            list_3 = list_3[:counter]
-            break
 
     return list_3 == list_1
 
 
 if __name__ == '__main__':
-    print(is_subsequence('acb', 'ahbgdc'))
+    print(is_subsequence('acb', 'ahbcgdbbc'))  # False
