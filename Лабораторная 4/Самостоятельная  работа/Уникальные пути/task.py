629d273ba34e97c1de0407a84180eec9d63b8552
alexceygryzilov <alexeygryzilov@gmail.com>
2024-06-21 12:58:28 +0300

    4.3

--- Лабораторная 4/Самостоятельная  работа/Уникальные пути/task.py
+++ Лабораторная 4/Самостоятельная  работа/Уникальные пути/task.py
@@ -1,12 +1,18 @@
 def unique_paths(m: int, n: int) -> int:
-    list_ = []
-    for i in range(num_rows):
-        nested_list = [1] * (i + 1)
-        list_.append(nested_list)
+    routes = [[0] * m for i in range(n)]
 
-    for i in range(2, len(list_)):
-        for j in range(1, len(list_[i]) - 1):
-            list_[i][j] = list_[i - 1][j - 1] + list_[i - 1][j]
-    if list_ is None:
-        return None
-    return list_[m + 1][n - 1]
+    for j in range(m):
+        routes[0][j] = 1
+    for i in range(1, n):
+        routes[i][0] = 1
+    for i in range(1, n):
+        for j in range(1, m):
+            routes[i][j] = routes[i - 1][j] + routes[i][j - 1]
+    for line in routes:
+        print(line)
+
+    return routes[-1][-1]
+
+
+if __name__ == '__main__':
+    print(unique_paths(3, 7))  # 28
